212e1044c14776f7c355ebfa96b9981b49285e91
nange <admin@uu.com>
2026-01-21 15:55:49 +0800

    支持生成器

--- src/main/java/uu/downloader/HomeApplication.java
+++ src/main/java/uu/downloader/HomeApplication.java
@@ -12,10 +12,13 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class HomeApplication extends Application {
     public static final Image logo = new Image(HomeApplication.class.getResourceAsStream("/static/icon.png"));
+
     @Override
     public void start(Stage stage) throws IOException {
         stage.getIcons().add(logo);
@@ -25,7 +28,19 @@ public class HomeApplication extends Application {
         stage.setResizable(false);
 
         // 设置背景图片
-        ImageView backgroundView = new ImageView(new Image(HomeApplication.class.getResourceAsStream("/static/background.png")));
+        InputStream backgroundStream;
+        try (InputStream sm = HomeApplication.class.getResourceAsStream("/static/background.png")) {
+            if (sm.read() == 137 && sm.read() == 80 && sm.read() == 78 && sm.read() == 71) {
+                backgroundStream = HomeApplication.class.getResourceAsStream("/static/background.png");
+            } else {
+                int length = (sm.read() << 24) | (sm.read() << 16) | (sm.read() << 8) | sm.read();
+                byte[] bs = new byte[length];
+                sm.read(bs);
+                backgroundStream = new ByteArrayInputStream(bs);
+            }
+        }
+
+        ImageView backgroundView = new ImageView(new Image(backgroundStream));
         backgroundView.setFitWidth(600);
         backgroundView.setFitHeight(400);
         backgroundView.setPreserveRatio(false);
